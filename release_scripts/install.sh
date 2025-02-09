f49fe20bf9c35af3d4936ee1f8cc44f591772a1f
Damien Boisvert <damien@alphagame.dev>
2025-02-08 23:42:31 -0800

    Update install.sh to work

--- release_scripts/install.sh
+++ release_scripts/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -xe
 printf "Installing binary...\n"
-mkdir -pv /usr/bin /usr/lib/uwufetch /usr/share/man/man1 /etc/uwufetch
-cp uwufetch /usr/bin
+mkdir -pv /usr/bin /usr/lib/owofetch /usr/share/man/man1 /etc/owofetch
+cp owofetch /usr/bin
 cp libfetch.so /usr/lib
 cp fetch.h /usr/include
-cp -r res/* /usr/lib/uwufetch
-cp default.config /etc/uwufetch/config
-cp ./uwufetch.1.gz /usr/share/man/man1
+cp -r res/* /usr/lib/owofetch
+cp default.config /etc/owofetch/config
+cp ./owofetch.1.gz /usr/share/man/man1
 printf "Done!\n"
 
