1046ee4b9b42dd22952cefacaae84927e6128f6b
Damien Boisvert <damien@alphagame.dev>
2025-02-15 00:40:10 -0800

    We now have a working uninstall script!
    you'll need it lol

--- release_scripts/uninstall.sh
+++ release_scripts/uninstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -xe
 printf "Uninstalling...\n"
-rm -f /usr/bin/uwufetch
-rm -rf /usr/lib/uwufetch
+rm -f /usr/bin/owofetch
+rm -rf /usr/lib/owofetch
 rm -f /usr/lib/libfetch.so
 rm -f /usr/include/fetch.h
-rm -rf /etc/uwufetch
-rm -f /usr/share/man/man1/uwufetch.1.gz
+rm -rf /etc/owofetch
+rm -f /usr/share/man/man1/owofetch.1.gz
 printf "Done!\n"
 
